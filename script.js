b0d9510174eb7b10af4d6e38715b68c6a2e721f6
Shrikanttathe-hub <142688445+Shrikanttathe-hub@users.noreply.github.com>
2023-10-30 01:44:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function decimalToBinary(num) {
   //Write you code here
 	let num = 13 , output = "" ;
-while(num >= 2) {
+    while(num >= 2) {
     let reminder = num % 2 ; 
     output = reminder + output;
     num = parseInt(num / 2) ;
@@ -9,7 +9,7 @@ while(num >= 2) {
 if(num === 1){
     output = 1 + output ;
 }
-console.log(output);
+//console.log(output);
   
 }
 
